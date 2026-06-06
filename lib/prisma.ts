6db8471799bd6b83bb2a9caf99fc1efb2c097b92
Bathiya Nethsara <157584911+bathiya2005@users.noreply.github.com>
2026-06-06 10:06:40 +0530

    fix prisma client

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { PrismaClient } from '@prisma/client'
 import { PrismaMariaDb } from '@prisma/adapter-mariadb'
 
@@ -6,13 +5,14 @@ const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
 
 function createPrismaClient() {
   const adapter = new PrismaMariaDb(process.env.DATABASE_URL!)
-  return new PrismaClient({ adapter, log: ['error'] })
+  return new PrismaClient({ adapter } as ConstructorParameters<typeof PrismaClient>[0])
 }
 
 export const prisma = globalForPrisma.prisma || createPrismaClient()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export async function getPrismaClient(): Promise<any> {
   return prisma
 }
